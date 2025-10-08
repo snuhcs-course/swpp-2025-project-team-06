d04407dd9cf29d2761f02a08dd7d78047ed4adfa
hotmilk <dbsthdud918@gmail.com>
2025-10-09 04:00:18 +0900

    build: modify dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation(libs.androidx.exifinterface)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
