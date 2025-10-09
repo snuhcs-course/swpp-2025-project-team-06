638167d9220e5adbddd2ad82c20c55ef60821efb
hyukjun <todd4@naver.com>
2025-10-09 14:21:28 +0900

    fix: rollback trailing slash

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -3,8 +3,8 @@ from .views import SignUpView, SignInView, SignOutView, TokenRefreshView
 
 app_name = 'accounts'
 urlpatterns = [
-    path("signup", SignUpView.as_view()), 
-    path("signin", SignInView.as_view()), 
-    path("signout", SignOutView.as_view()), 
-    path("refresh", TokenRefreshView.as_view()),
+    path("signup/", SignUpView.as_view()), 
+    path("signin/", SignInView.as_view()), 
+    path("signout/", SignOutView.as_view()), 
+    path("refresh/", TokenRefreshView.as_view()),
 ]
