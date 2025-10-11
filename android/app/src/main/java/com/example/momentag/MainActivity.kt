238056ff8fe7cd0fbfef5e65158adb5c3c7193c1
MMking7 <smk1557@naver.com>
2025-10-11 21:57:02 +0900

    chore(merge): MainActivity.kt

--- android/app/src/main/java/com/example/momentag/MainActivity.kt
+++ android/app/src/main/java/com/example/momentag/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.momentag.ui.theme.MomenTagTheme
 
-class  git rev-parse --verify 980a169870fadb31092f7bc6b5b87044d07708fbMainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
