2d6b5ec21c1aaaa6311d83cb84109ff14289b824
jukebox03 <jukebox0312@gmail.com>
2025-10-22 15:02:09 +0900

    feat: caption RDB, RepVec RDB

--- backend/gallery/models.py
+++ backend/gallery/models.py
@@ -21,8 +21,8 @@ class Photo_Tag(models.Model):
     def __str__(self):
         return f"{self.photo_id} tagged with {self.tag_id}"
     
-class Photo_Caption(models.Model):
-    pc_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+class Caption_Tag(models.Model):
+    ct_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     
     photo_id = models.UUIDField(max_length=255, unique=True)
@@ -39,3 +39,13 @@ class Caption(models.Model):
 
     def __str__(self):
         return self.caption
+    
+class RepVec_Tag(models.Model):
+    rpt_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    vec_id = models.UUIDField(max_length=255, unique=True)
+    tag_id = models.UUIDField(max_length=255)
+
+    def __str__(self):
+        return f"{self.photo_id} is representative vector of {self.tag_id}"
