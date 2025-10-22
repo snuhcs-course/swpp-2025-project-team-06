5d3cff75c4b2df6a8766cc003568a423f93c493d
jukebox03 <jukebox0312@gmail.com>
2025-10-22 16:21:45 +0900

    fix: MainCoroutineRule format error

--- android/app/src/test/java/com/example/momentag/MainCoroutineRule.kt
+++ android/app/src/test/java/com/example/momentag/MainCoroutineRule.kt
@@ -2,8 +2,8 @@ package com.example.momentag.viewmodel
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
