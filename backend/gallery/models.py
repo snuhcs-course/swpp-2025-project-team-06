b624c5fc3f2ef46e6fe0009e763aa09a3010b97e
jukebox03 <jukebox0312@gmail.com>
2025-10-22 20:38:53 +0900

    refactor: Photo_Caption DB rename

--- backend/gallery/models.py
+++ backend/gallery/models.py
@@ -22,3 +22,31 @@ class Photo_Tag(models.Model):
     def __str__(self):
         return f"{self.photo_id} tagged with {self.tag_id}"
     
+class Photo_Caption(models.Model):
+    ct_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    photo_id = models.UUIDField(max_length=255, unique=True)
+    caption_id = models.UUIDField(max_length=255)
+
+    def __str__(self):
+        return f"{self.photo_id} captioned with {self.caption_id}"
+    
+class Caption(models.Model):
+    caption_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    caption= models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.caption
+    
+class RepVec_Tag(models.Model):
+    rpt_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    vec_id = models.UUIDField(max_length=255, unique=True)
+    tag_id = models.UUIDField(max_length=255)
+
+    def __str__(self):
+        return f"{self.photo_id} is representative vector of {self.tag_id}"
