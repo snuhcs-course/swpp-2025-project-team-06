65adad7ccfdbb5e7f18459066aa094b110072e52
Hyukjun Kwon <todd4@naver.com>
2025-11-08 22:59:31 +0900

    feat: make story api use db=0 of redis

--- backend/config/redis.py
+++ backend/config/redis.py
@@ -11,6 +11,6 @@ def get_redis():
             port=settings.REDIS_PORT,
             password=settings.REDIS_PASSWORD,
             decode_responses=True,
-            db=1
+            db=0
         )
     return _redis
