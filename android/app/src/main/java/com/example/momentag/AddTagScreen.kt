b893435df886145a762f07338cafb4078f9a37f1
WallabyG <spearmint1022@gmail.com>
2025-11-08 20:22:56 +0900

    chore: remove unused import

--- android/app/src/main/java/com/example/momentag/AddTagScreen.kt
+++ android/app/src/main/java/com/example/momentag/AddTagScreen.kt
@@ -58,7 +58,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
