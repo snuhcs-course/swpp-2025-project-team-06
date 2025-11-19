f86e70341f4fe2cf7119611836e3230e9408874c
MMking7 <smk1557@naver.com>
2025-11-19 19:56:38 +0900

    chore : minor ktlint issue

--- android/app/src/main/java/com/example/momentag/ui/components/ErrorDialog.kt
+++ android/app/src/main/java/com/example/momentag/ui/components/ErrorDialog.kt
@@ -31,7 +31,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.example.momentag.ui.theme.Dimen
