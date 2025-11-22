84f1ff53a1aaa3bc34d534616d3527b71f4018fe
hotmilk <dbsthdud918@gmail.com>
2025-11-22 12:15:05 +0900

    chore: run ktlint

--- android/app/src/main/java/com/example/momentag/view/AddTagScreen.kt
+++ android/app/src/main/java/com/example/momentag/view/AddTagScreen.kt
@@ -58,7 +58,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
